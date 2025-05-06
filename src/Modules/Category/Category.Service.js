ddf9b1c93abf095be3fbb5de3470d23cf1b719e9
mehrzad <mehrzad20061384@gmail.com>
2025-05-06 10:48:53 +0330

    cat

--- src/Modules/Category/Category.Service.js
+++ src/Modules/Category/Category.Service.js
@@ -1,7 +1,8 @@
-import { postgresQlClient } from "../../config/postgres.js";
-import { categoryValidation } from "./validation.js";
+import { query } from "../../Configs/PostgresQl.js";
 import createHttpError from "http-errors";
 import { makeSlug } from "../../Helpers/Helper.js";
+import uploadQueue from "../../Queues/UpoladQueue.js";
+import { categoryValidation } from "./validation.js";
 
 
 export const indexService = async (req, res, next) => {
@@ -34,6 +35,7 @@ export const indexService = async (req, res, next) => {
 
 }
 
+
 export const storeService = async (req, res, next) => {
 
     try {
@@ -41,12 +43,26 @@ export const storeService = async (req, res, next) => {
         const { error } = categoryValidation.validate(req.body);
         if(error) next(createHttpError.BadRequest(error.message));
 
-        const { name, description } = req.body;
+        const { name, translations, status, icon = null } = req.body;
 
         const slug = await makeSlug(name,"categories");
 
-        const query = "insert into categories (name,slug,description) values ($1, $2, $3) returning *";
-        const result = await postgresQlClient.query(query, [name, slug, description]);
+        const sql = "insert into categories (name,slug,description,status,icon) values ($1, $2, $3, $4, $5) returning *";
+        const result = await query(sql, [name, slug, translations, status, icon]);
+
+        if(!result.rows[0]) next(createHttpError.BadRequest("Category not created"));
+
+        if(req.file && req.file.image) {
+            await uploadQueue.add("upload", {
+                files: [req.file?.image],
+                table: "categories",
+                img_field: "image",
+                data: {
+                    id: result.rows[0].id,
+                    image: req.file.image,
+                },
+            });
+        }
 
         res.status(200).json({
             data: result.rows[0],
@@ -66,8 +82,8 @@ export const showService = async (req, res, next) => {
 
         const { slug } = req.params;
 
-        const query = "select * from categories where slug = $1";
-        const result = await postgresQlClient.query(query, [slug]);
+        const sql = "select * from categories where slug = $1";
+        const result = await query(sql, [slug]);
 
         if(!result.rows[0]) next(createHttpError.NotFound("Category not found"));
 
@@ -92,13 +108,33 @@ export const updateService = async (req, res, next) => {
         const { error } = categoryValidation.validate(req.body);
         if(error) next(createHttpError.BadRequest(error.message));
 
-        const category = await postgresQlClient.query("select * from categories where slug = $1", [slug]);
+        const category = await query("select * from categories where slug = $1", [slug]);
         if(!category.rows[0]) next(createHttpError.NotFound("Category not found"));
 
         const { name, description } = req.body;
 
-        const query = "update categories set name = $1, description = $2 where slug = $3 returning *";
-        const result = await postgresQlClient.query(query, [name, description, slug]);
+        const sql = "update categories set name = $1, description = $2 where slug = $3 returning *";
+        const result = await query(sql, [name, description, slug]);
+
+        if(!result.rows[0]) next(createHttpError.BadRequest("Category not updated"));
+
+        if(req.file && req.file.image) {
+
+            await uploadQueue.add("deleteFile", {
+                file: category.rows[0].image,
+            });
+
+            await uploadQueue.add("upload", {
+                files: [req.file?.image],
+                table: "categories",
+                img_field: "image",
+                data: {
+                    id: category.rows[0].id,
+                    image: req.file.image,
+                },
+            });
+
+        }
 
         res.status(200).json({
             data: result.rows[0],
@@ -118,11 +154,11 @@ export const destroyService = async (req, res, next) => {
 
         const { slug } = req.params;
 
-        const category = await postgresQlClient.query("select * from categories where slug = $1", [slug]);
+        const category = await query("select * from categories where slug = $1", [slug]);
         if(!category.rows[0]) next(createHttpError.NotFound("Category not found"));
 
-        const query = "delete from categories where slug = $1";
-        await postgresQlClient.query(query, [slug]);
+        const sql = "delete from categories where slug = $1";
+        await query(sql, [slug]);
 
         res.status(200).json({
             success: true,
@@ -141,11 +177,11 @@ export const changeStatusService = async (req, res, next) => {
 
         const { slug } = req.params;
 
-        const category = await postgresQlClient.query("select * from categories where slug = $1", [slug]);
+        const category = await query("select * from categories where slug = $1", [slug]);
         if(!category.rows[0]) next(createHttpError.NotFound("Category not found"));
 
-        const query = "update categories set status = $1 where slug = $2";
-        await postgresQlClient.query(query, [category.rows[0].status === 1 ? 0 : 1, slug]);
+        const sql = "update categories set status = $1 where slug = $2";
+        await query(sql, [category.rows[0].status === 1 ? 0 : 1, slug]);
 
         res.status(200).json({
             success: true,
--- src/Modules/Category/validation.js
+++ src/Modules/Category/validation.js
@@ -2,5 +2,10 @@ import Joi from "joi";
 
 export const categoryValidation = Joi.object({
     name: Joi.string().required(),
-    description: Joi.string().required(),
+    translations: Joi.alternatives().try(
+        Joi.object().pattern(Joi.string(), Joi.string()), // { en: "Hello" }
+        Joi.string().json() // '{"en":"Hello"}'
+    ).default({}),
+    status: Joi.number().required().valid(0, 1),
+    icon: Joi.string().required(),
 });
